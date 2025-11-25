5fbee91a35ad837c9db22c762418ac0534c2d6a1
hulumbulum <oksanaanciferova53@gmail.com>
2025-11-25 18:56:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/budgetmaster/', // ЗАМЕНИТЕ на имя вашего репозитория
+  base: '/budgetmaster/',
   build: {
     outDir: 'dist',
-    sourcemap: true
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   },
-  server: {
-    port: 3000,
-    open: true
-  }
+  publicDir: 'public'
 })
